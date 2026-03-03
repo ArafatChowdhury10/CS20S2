579ec0bc2838f4c43aee0aa6fbcb701a4065d24f
Old PC <Old PC@Arafat>
2026-03-02 23:52:01 -0700

    11:47pm

--- Chapter4/src/Mastery/Grade.java
+++ Chapter4/src/Mastery/Grade.java
@@ -12,21 +12,42 @@ public class Grade
 		System.out.print("Enter the percentage: ");
 		
 		int percentage = input.nextInt();
-		
+	
+		//If percentage is lower than 59 percent
 		if (percentage <= 59)
-		System.out.println("The Corresponding letter grade is: F");
+		{
+			System.out.println("The Corresponding letter grade is: F");			
+		}
 		
+		//If percentage is lower than 69 percent
 		else if (percentage <= 69)
-		System.out.println("The Corresponding letter grade is: D");
+		{
+			System.out.println("The Corresponding letter grade is: D");
+		}
 		
+		//If percentage is lower than 79 percent
 		else if (percentage <= 79)
-		System.out.println("The Corresponding letter grade is: C");
+		{
+			System.out.println("The Corresponding letter grade is: C");
+		}
 		
+		//If percentage is lower than 89 percent
 		else if (percentage <= 89)
-		System.out.println("The Corresponding letter grade is: B");
+		{
+			System.out.println("The Corresponding letter grade is: B");
+		}
 		
+		//If percentage is lower than 100 percent
 		else if (percentage <= 100)
-		System.out.println("The Corresponding letter grade is: A");
+		{
+			System.out.println("The Corresponding letter grade is: A");
+		}
+		
+		//If the user puts an invalid percentage, display this message
+		else
+		{
+			System.out.println("Invalid percentage!");	
+		}
 		
 	}
 
