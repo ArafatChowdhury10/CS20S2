aee40d26c034e92c140ff46915723d9c8b2dbf51
49105004 <49105004@L815WD220025.edu.cbe.ab.ca>
2026-03-04 14:04:29 -0700

    2:04pm

--- Chapter4/src/Mastery/QuadraticEquation.java
+++ Chapter4/src/Mastery/QuadraticEquation.java
@@ -11,48 +11,64 @@ Course: Computer Programming 20
 */
 
 package Mastery;
+
 import java.util.Scanner;
+
 public class QuadraticEquation
 {
 	public static void main(String[] args)
 	{
+		// Create a Scanner object to read user input
 		Scanner input = new Scanner(System.in);
 		
+		// Ask the user to enter the coefficient "a"
 		System.out.print("Enter a value for A: ");
 		double a = input.nextDouble();
 		
+		// Ask the user to enter the coefficient "b"
 		System.out.print("Enter a value for B: ");
 		double b = input.nextDouble();
 		
+		// Ask the user to enter the coefficient "c"
 		System.out.print("Enter a value for C: ");
 		double c = input.nextDouble();
 		
-		// This calculates the discriminant as b^2 - 4*a*c
+		// Calculate the discriminant using the formula b^2 - 4ac
+		// The discriminant determines how many real roots exist
 		double calculateDiscriminant = b * b - 4 * a * c;
 		
-		// Calculate -b +- the square root of the discriminant
+		// Calculate the numerator portion of the quadratic formula:
+		// -b ± √(discriminant)
 		double root1 = -b - Math.sqrt(calculateDiscriminant);
 		double root2 = -b + Math.sqrt(calculateDiscriminant);
 		
-		// If there is only 2 real roots
+		// If the discriminant is greater than 0,
+		// there are two different real roots
 		if (calculateDiscriminant > 0)
 		{
+			// Divide each numerator by 2a to get the final roots
 			double dividing = root1 / (2 * a);
 			double dividing2 = root2 / (2 * a);
+			
+			// Display both roots
 			System.out.println("The roots are " + dividing + " and " + dividing2);
 		}
-		// If there is only 1 real root
+		// If the discriminant equals 0,
+		// there is exactly one real root
 		else if (calculateDiscriminant == 0)
 		{
+			// Calculate the single root
 			double root3 = -b / (2 * a);
+			
+			// Display the root
 			System.out.println("The root is " + root3);
 		}
-		// If there are no real roots
+		// If the discriminant is less than 0,
+		// there are no real roots (only complex roots)
 		else
 		{
 			System.out.println("There are no real roots ");
 		}
-		
 	}
 }
 
