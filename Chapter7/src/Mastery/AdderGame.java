8f096bc7d473c5c1a928fd382b015a2ef9e1d4fd
49105004 <49105004@L815WD220025.edu.cbe.ab.ca>
2026-04-23 15:15:11 -0600

    3:15pm

--- Chapter7/src/Mastery/AdderGame.java
+++ Chapter7/src/Mastery/AdderGame.java
@@ -5,14 +5,29 @@ import java.util.Random;
 
 public class AdderGame 
 {
-
-	public static void main(String[] args) 
+	private int score;
+	private Scanner input;
+	
+	public AdderGame()
 	{
-		Scanner input = new Scanner(System.in);
-		
-		
-
-
+		score = 0; 
+		input = new Scanner(System.in);
 	}
-
+	
+	public void startGame()
+	{
+		while(true)
+		{
+			Question question = new Question();
+			int tries = 0;
+			int user = input.nextInt();
+			while(tries < 3)
+			{
+				if (user == 999)
+				{
+					System.out.println("Your score is " + score);
+				}
+			}
+		}
+	}
 }
